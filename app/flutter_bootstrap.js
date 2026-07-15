1dca386f0f023bc8183b98c613873acfef73a086
Keith Parsons <keith@wlanpros.com>
2026-07-15 16:21:33 -0400

    Deploy Toolbox web 1.7.4 (build 26071500), base href /app/

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1944204741" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "766760790" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
